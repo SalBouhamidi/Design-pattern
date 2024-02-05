6d942ac8ff88283c9fd0ce0dedf77f2a91026a9b
SalBouhamidi <bouhamidi.sal@gmail.com>
2024-02-05 10:58:25 +0100

    singleton allows me TO instance an object

--- singleton/Singleton.php
+++ singleton/Singleton.php
@@ -1,12 +1,37 @@
 <?php
     class Singloton
     {
-        private static $object;
+        private static $instance;
+        private $connect;
+        private $username ="root";
+        private $dbname;
+        private $pwd="";
+        private $host="localhost";
 
         private function __construct(){
 
+        try {
+            $this->connect = new PDO("mysql:host=localhost;dbname=crudd", $this->username, $this->pwd);
+            // set the PDO error mode to exception
+            $this->connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "Connected successfully";
+          } catch(PDOException $e) {
+            echo "Connection failed: " . $e->getMessage();
+          }
         }
-
+        public static function getInstance()
+        {
+            if (self::$instance === null) {
+                self::$instance = new self();
+            }
+            
+            return self::$instance;
+        }
     }
+$singleobject = Singloton:: getInstance();
+
+$singleobject2 = Singloton:: getInstance();
+var_dump($singleobject2);
+die();
 
 ?>
